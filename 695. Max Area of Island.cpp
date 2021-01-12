6d7b38c57a876afcb1d1395a85c9d9d34b26edf3
Qiaoyu Gu <qiaoyu.gu@njust.edu.cn>
2021-01-12 20:49:59 +0800

    695

--- 695. Max Area of Island.cpp
+++ 695. Max Area of Island.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>>& grid, int& res, int i, int j) {
+        int m = grid.size();
+        int n = grid[0].size();
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) {
+            return;
+        }
+        grid[i][j] = 0;
+        res ++;
+        dfs(grid, res, i + 1, j);
+        dfs(grid, res, i - 1, j);
+        dfs(grid, res, i, j + 1);
+        dfs(grid, res, i, j - 1);
+        return;
+
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        if(grid.empty()==true)
-            return 0;
-        int res=0;
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++)
-            {
-                if(grid[i][j]==0)
-                    continue;
-                if(grid[i][j]==1)
-                {
-                    int cur=0;
-                    dfs(grid,i,j,m,n,cur);
-                    res=max(res,cur);
+        int m = grid.size();
+        int n = grid[0].size();
+        int res = 0;
+        for (int i = 0; i < m; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (grid[i][j] == 1) {
+                    int cur = 0;
+                    dfs(grid, cur, i, j);
+                    res = max(res, cur);
                 }
             }
+        }
         return res;
-        
-    }
-private:
-    void dfs(vector<vector<int>>& grid, int i, int j,int m, int n, int& cur)
-    {
-        if(i<0||i>=m||j<0||j>=n||grid[i][j]==0)
-            return;
-            cur++;
-        grid[i][j]=0;
-        dfs(grid,i-1,j,m,n,cur);
-        dfs(grid,i+1,j,m,n,cur);
-        dfs(grid,i,j-1,m,n,cur);
-        dfs(grid,i,j+1,m,n,cur);            
     }
 };
-
-//一定要记住在dfs函数里把遍历过的点归零！！！！
