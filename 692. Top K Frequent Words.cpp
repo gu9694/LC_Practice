38ad846646e45b8c5613b74c35f5ee1a79c0861f
Qiaoyu Gu <qiaoyu.gu@njust.edu.cn>
2021-01-12 21:06:00 +0800

    692

--- 692. Top K Frequent Words.cpp
+++ 692. Top K Frequent Words.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    struct myless{
-        bool operator()(pair<int,string> a, pair<int,string> b){
-            return a.first<b.first||(a.first==b.first&&a.second>b.second);
+    struct myless {
+        bool operator()(pair<int, string>& a, pair<int, string>& b) {
+            if (a.first == b.first) {
+                return a.second > b.second;
+            } else return a.first < b.first;
         }
-        
     };
+
     vector<string> topKFrequent(vector<string>& words, int k) {
         vector<string> res;
-        unordered_map<string,int> mymap;
-        priority_queue<pair<int,string>,vector<pair<int,string>>,myless> mypq;
-        int len=words.size();
-        for(int i=0;i<len;i++)
-        {
-            mymap[words[i]]++;
+        unordered_map<string, int> mymap;
+        for (auto i : words) {
+            mymap[i] ++;
         }
-        for(auto i: mymap)
-        {
-            mypq.push(make_pair(i.second,i.first));
+        priority_queue<pair<int, string>, vector<pair<int, string>>, myless> mypq;
+        for (auto i : mymap) {
+            mypq.push({i.second, i.first});
         }
-        for(int i=1;i<=k;i++)
-        {
-            string tmp=mypq.top().second;
+        for (int i = 0; i < k; i ++) {
+            res.push_back(mypq.top().second);
             mypq.pop();
-            res.push_back(tmp);
         }
-        return res;
         
+        return res;
     }
 };
-
-//先用hashmap存input
-//然后用pq排hashmap
-//pq中myless的定义：用结构体重载()，pq里定义myless居然不用加上()
-//和973对比
